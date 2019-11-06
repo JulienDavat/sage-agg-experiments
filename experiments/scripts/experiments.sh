810e9a60547fd9cdd1cad4eb1a8976b8c2adf73a
Grall Arnaud <folkvir@outlook.fr>
2019-11-06 16:20:28 +0100

    fix 5000 results

--- experiments/scripts/experiments.sh
+++ experiments/scripts/experiments.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 CUR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+ADDR=$1
+
 # bsbm10 experiment
 rm -rf $CUR/../../output/bsbm10
 bash $CUR/experiment-bis.sh "bsbm10" \
-    "http://172.16.8.50:7130/sparql/" \
-    "http://localhost:7120/sparql/bsbm10" \
+    "http://$ADDR:7130/sparql/" \
+    "http://$ADDR:7120/sparql/bsbm10" \
     "$CUR/../../build/libs/sage-sparql-void-fat-1.0.jar" \
     "http://sage.univ-nantes.fr/bsbm10" \
     "$CUR/../../data/queries/queries-wo-construct.txt"
@@ -13,8 +15,8 @@ bash $CUR/experiment-bis.sh "bsbm10" \
 # bsbm100 experiment
 rm -rf $CUR/../../output/bsbm100
 bash $CUR/experiment-bis.sh "bsbm100" \
-    "http://localhost:7130/sparql/" \
-    "http://localhost:7120/sparql/bsbm100" \
+    "http://$ADDR:7130/sparql/" \
+    "http://$ADDR:7120/sparql/bsbm100" \
     "$CUR/../../build/libs/sage-sparql-void-fat-1.0.jar" \
     "http://sage.univ-nantes.fr/bsbm100" \
     "$CUR/../../data/queries/queries-wo-construct.txt"
@@ -22,8 +24,8 @@ bash $CUR/experiment-bis.sh "bsbm100" \
 # bsbm1k experiment
 rm -rf $CUR/../../output/bsbm1k
 bash $CUR/experiment-bis.sh "bsbm1k" \
-    "http://localhost:7130/sparql/" \
-    "http://localhost:7120/sparql/bsbm1k" \
+    "http://$ADDR:7130/sparql/" \
+    "http://$ADDR:7120/sparql/bsbm1k" \
     "$CUR/../../build/libs/sage-sparql-void-fat-1.0.jar" \
     "http://sage.univ-nantes.fr/bsbm1k" \
     "$CUR/../../data/queries/queries-wo-construct.txt"
