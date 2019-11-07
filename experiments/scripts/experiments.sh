e98475f05c4d806451a3956bd7cce645ba006d9c
Grall Arnaud <folkvir@outlook.fr>
2019-11-07 20:14:06 +0100

    virtuoso/sage normal/sage optimized

--- experiments/scripts/experiments.sh
+++ experiments/scripts/experiments.sh
@@ -2,6 +2,8 @@
 CUR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ADDR=$1
+RUNS=$2
+QUERIES=$3
 
 # bsbm10 experiment
 rm -rf $CUR/../../output/bsbm10
@@ -10,7 +12,8 @@ bash $CUR/experiment-bis.sh "bsbm10" \
     "http://$ADDR:7120/sparql/bsbm10" \
     "$CUR/../../build/libs/sage-sparql-void-fat-1.0.jar" \
     "http://sage.univ-nantes.fr/bsbm10" \
-    "$CUR/../../data/queries/queries-wo-construct.txt"
+    "$CUR/../../data/queries/queries-wo-construct$QUERIES.txt" \
+    $RUNS
 
 # bsbm100 experiment
 rm -rf $CUR/../../output/bsbm100
@@ -19,7 +22,8 @@ bash $CUR/experiment-bis.sh "bsbm100" \
     "http://$ADDR:7120/sparql/bsbm100" \
     "$CUR/../../build/libs/sage-sparql-void-fat-1.0.jar" \
     "http://sage.univ-nantes.fr/bsbm100" \
-    "$CUR/../../data/queries/queries-wo-construct.txt"
+    "$CUR/../../data/queries/queries-wo-construct$QUERIES.txt" \
+    $RUNS
 
 # bsbm1k experiment
 rm -rf $CUR/../../output/bsbm1k
@@ -28,6 +32,7 @@ bash $CUR/experiment-bis.sh "bsbm1k" \
     "http://$ADDR:7120/sparql/bsbm1k" \
     "$CUR/../../build/libs/sage-sparql-void-fat-1.0.jar" \
     "http://sage.univ-nantes.fr/bsbm1k" \
-    "$CUR/../../data/queries/queries-wo-construct.txt"
+    "$CUR/../../data/queries/queries-wo-construct$QUERIES.txt" \
+    $RUNS
 
 
