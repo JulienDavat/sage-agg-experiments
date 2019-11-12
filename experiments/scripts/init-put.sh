bf4748b2c1051e986eaff6a56f360e861a4cfa4b
Grall Arnaud <folkvir@outlook.fr>
2019-11-12 16:24:59 +0100

    updtae init script

--- experiments/scripts/init-put.sh
+++ experiments/scripts/init-put.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
 # init
-docker exec -t sage-agg sage-postgres-init /opt/data/datasets/experiments.yaml bsbm1k
-# add
-docker exec -t sage-agg sage-postgres-put --format ttl /opt/data/datasets/bsbm1k.ttl /opt/data/experiments.yaml bsbm1k
-
-# init
 docker exec -t sage-agg sage-postgres-init /opt/data/experiments.yaml bsbm10
 # add
 docker exec -t sage-agg sage-postgres-put --format ttl /opt/data/datasets/bsbm10.ttl /opt/data/experiments.yaml bsbm10
+
+# init
+docker exec -t sage-agg sage-postgres-init /opt/data/experiments.yaml bsbm100
+# add
+docker exec -t sage-agg sage-postgres-put --format ttl /opt/data/datasets/bsbm100.ttl /opt/data/experiments.yaml bsbm100
 
 
 # init
 docker exec -t sage-agg sage-postgres-init /opt/data/experiments.yaml bsbm1k
 # add
-docker exec -t sage-agg sage-postgres-put --format ttl /opt/data/datasets/bsbm100.ttl /opt/data/experiments.yaml bsbm100
-
-# init
-docker exec -t sage-agg sage-postgres-init /opt/data/experiments.yaml dbpedia351
-# add
-docker exec -t sage-agg sage-postgres-put --format ttl /opt/data/datasets/dbpedia351.ttl /opt/data/experiments.yaml dbpedia351
+docker exec -t sage-agg sage-postgres-put --format ttl /opt/data/datasets/bsbm1k.ttl /opt/data/experiments.yaml bsbm1k
