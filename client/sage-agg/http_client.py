ef088ba07b16ebfc998f0c2dc01e8d6672b1d39a
JulienDavat <julien.aimonierdavat@gmail.com>
2021-01-23 13:26:36 +0100

    Retry policy
    Adds a retry policy to the http module of the sage-agg client

--- client/sage-agg/http_client.py
+++ client/sage-agg/http_client.py
@@ -3,36 +3,45 @@ import sys
 import logging
 import coloredlogs
 import requests
-from time import time
+from time import time, sleep
 
 coloredlogs.install(level='INFO', fmt='%(asctime)s - %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
 
+def send_request(server, payload, retry=100):
+    try:
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            'Cache-Control': 'no-cache',
+            "next": None
+        }
+        response = requests.post(server, headers=headers, data=json.dumps(payload))
+        return response.json()
+    except:
+        if retry > 0:
+            sleep(0.5)
+            return send_request(server, payload, retry=retry - 1)
+        else:
+            raise Exception('A network error occured...')
+
 def execute(server, dataset, query, accumulator, spy):
     has_next = True
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        'Cache-Control': 'no-cache',
-        "next": None
-    }
     payload = {
         "query": query,
         "defaultGraph": dataset,
         "next": None
     }
-
     start = time()
     while has_next:
-        response = requests.post(server, headers=headers, data=json.dumps(payload))
-        json_response = response.json()
-        has_next = json_response['hasNext']
-        payload["next"] = json_response["next"]
+        response = send_request(server, payload)
+        has_next = response['hasNext']
+        payload["next"] = response["next"]
         
         spy.report_nb_calls(1)
-        spy.report_data_transfer(sys.getsizeof(json.dumps(json_response)))
+        spy.report_data_transfer(sys.getsizeof(json.dumps(response)))
         
-        for bindings in json_response['bindings']:
+        for bindings in response['bindings']:
             logger.debug(bindings)
             accumulator.accumulate(bindings)
     execution_time = time() - start
